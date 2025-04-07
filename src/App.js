20df28a505d8744696467eab148189b9be65a88c
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2025-04-07 19:50:35 -0400

    Add a little bit of css

--- src/App.js
+++ src/App.js
@@ -11,60 +11,51 @@ function App() {
         </p>
         <p>
           This is a simple form to collect information about your recent hangouts.
-        </p>
-        <p>
           Please fill out the form below to get started.
         </p>
-
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name"/>
-          </label>
+        </header>
+        <form action="https://example.com/submit" method="POST">
+          <label>Your Name: </label>
+            <input type="text" name="name" placeholder='John Doe'/>
           <br/>
-          <br/>
-          <label>
-            Date of Hangout:
+          <label>Date of Hangout:</label>
             <input type="date" name="date"/>
-          </label>
           <br/>
-          <label>
-            did i act okay?
-            <input type="radio" name="act" value="yes"/> yes
-            <input type="radio" name="act" value="no"/> no
-          </label>
+          <label>Did i act okay?</label>
+          <select name="did-i-act-okay">
+            <option value="yes">yes</option>
+            <option value="no">no</option>
+          </select>
           <br/>
-          <label>
-            was i cool?
-            <input type="radio" name="cool" value="yes"/> yes
-            <input type="radio" name="cool" value="no"/> no
-          </label>
+          <label>Was i cool?</label>
+          <select name="was-i-cool">
+            <option value="yes">yes</option>
+            <option value="no">no</option>
+          </select>
           <br/>
-          <label>
-            was i fun?
-            <input type="radio" name="fun" value="yes"/> yes
-            <input type="radio" name="fun" value="no"/> no
-          </label>
+          <label>Was i fun?</label>
+          <select name="was-i-fun">
+            <option value="yes">yes</option>
+            <option value="no">no</option>
+          </select>
           <br/>
-          <label>
-            did you have fun?
-            <input type="radio" name="did-you-fun" value="yes"/> yes
-            <input type="radio" name="did-you-fun" value="no"/> no
-          </label>
+          <label>Did you have fun?</label>
+          <select name="did-you-have-fun">
+            <option value="yes">yes</option>
+            <option value="no">no</option>
+          </select>
           <br/>
-          <label>
-            did i do anything wrong?
-            <input type="radio" name="wrong" value="yes"/> yes
-            <input type="radio" name="wrong" value="no"/> no
-          </label>
+          <label>Did i do anything wrong?</label>
+          <select name="did-i-do-anything-wrong">
+            <option value="yes">yes</option>
+            <option value="no">no</option>
+          </select>
           <br/>
-          <button type="submit">Submit</button>
-        </form>
+          <button class="boutton" type="submit" onclick="return soumettreformulaire()">Submit</button>
+          </form>
         <p>
           Thank you for your participation!
         </p>
-
-      </header>
     </div>
   );
 }
