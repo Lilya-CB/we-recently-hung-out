38bcd8ae5297878ba6c592ea50c387b2b24e5c08
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2025-04-07 18:14:00 -0400

    basic form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
+import logo from './img/logo.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, we recently hung out!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          This is a simple form to collect information about your recent hangouts.
+        </p>
+        <p>
+          Please fill out the form below to get started.
+        </p>
+
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name"/>
+          </label>
+          <br/>
+          <br/>
+          <label>
+            Date of Hangout:
+            <input type="date" name="date"/>
+          </label>
+          <br/>
+          <label>
+            did i act okay?
+            <input type="radio" name="act" value="yes"/> yes
+            <input type="radio" name="act" value="no"/> no
+          </label>
+          <br/>
+          <label>
+            was i cool?
+            <input type="radio" name="cool" value="yes"/> yes
+            <input type="radio" name="cool" value="no"/> no
+          </label>
+          <br/>
+          <label>
+            was i fun?
+            <input type="radio" name="fun" value="yes"/> yes
+            <input type="radio" name="fun" value="no"/> no
+          </label>
+          <br/>
+          <label>
+            did you have fun?
+            <input type="radio" name="did-you-fun" value="yes"/> yes
+            <input type="radio" name="did-you-fun" value="no"/> no
+          </label>
+          <br/>
+          <label>
+            did i do anything wrong?
+            <input type="radio" name="wrong" value="yes"/> yes
+            <input type="radio" name="wrong" value="no"/> no
+          </label>
+          <br/>
+          <button type="submit">Submit</button>
+        </form>
+        <p>
+          Thank you for your participation!
+        </p>
+
       </header>
     </div>
   );
